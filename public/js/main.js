19e297f03a73b6bf021ae5b922850c542bf907c1
Eckon <niklas.meyer95@hotmail.de>
2018-10-14 00:23:13 +0200

    highlight the header if no sub-page is selected

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,27 @@
-// main nav redirect on click
+// main nav redirect on click and color the header if no highlight is visible
 (function () {
+    let highlighted = false;
+
     let items = document.querySelector('#main-nav').querySelectorAll('div');
     items.forEach((ele) => {
+        // if there is one highlighted -> don't highlight the header
+        ele.parentNode.classList.forEach((c) => {
+            if (c === 'current-page') {
+                highlighted = true;
+            }
+        });
+
+        // add event listener to all buttons
         ele.parentNode.addEventListener('click', () => {
             window.location.href = ele.dataset.path;
         })
-    })
+    });
+
+    if (! highlighted) {
+        const highlightColor = getComputedStyle(document.body).getPropertyValue('--main-hover-color');
+        const header = document.querySelector('#header-title').querySelector('a');
+        header.style.color = highlightColor;
+    }
 })();
 
 // show the vertical nav bar when click action on small-nav-header (when screen is small - mobile)
@@ -14,5 +30,5 @@
     nav.addEventListener('click', () => {
         const s = document.querySelector('#main-nav').style;
         s.display = s.display === 'block' ? 'none' : 'block';
-    })
+    });
 })();
