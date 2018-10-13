c2f84555ac33f586b8a54f27976f1938599e0660
Eckon <niklas.meyer95@hotmail.de>
2018-10-14 00:34:49 +0200

    highlight the header - update (with golang instead of js -> better)

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,12 @@
-// main nav redirect on click and color the header if no highlight is visible
+// main nav redirect on click
 (function () {
-    let highlighted = false;
-
     let items = document.querySelector('#main-nav').querySelectorAll('div');
     items.forEach((ele) => {
-        // if there is one highlighted -> don't highlight the header
-        ele.parentNode.classList.forEach((c) => {
-            if (c === 'current-page') {
-                highlighted = true;
-            }
-        });
-
         // add event listener to all buttons
         ele.parentNode.addEventListener('click', () => {
             window.location.href = ele.dataset.path;
         })
     });
-
-    if (! highlighted) {
-        const highlightColor = getComputedStyle(document.body).getPropertyValue('--main-hover-color');
-        const header = document.querySelector('#header-title').querySelector('a');
-        header.style.color = highlightColor;
-    }
 })();
 
 // show the vertical nav bar when click action on small-nav-header (when screen is small - mobile)
