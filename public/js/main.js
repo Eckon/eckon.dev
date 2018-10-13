623f61b313c373f6f39d652fd445906134be1da3
Eckon <niklas.meyer95@hotmail.de>
2018-10-13 23:55:44 +0200

    added nav bar for mobile view (vertical with hide/show click action)

--- public/js/main.js
+++ public/js/main.js
@@ -7,3 +7,12 @@
         })
     })
 })();
+
+// show the vertical nav bar when click action on small-nav-header (when screen is small - mobile)
+(function () {
+    let nav = document.querySelector('#small-nav-header');
+    nav.addEventListener('click', () => {
+        const s = document.querySelector('#main-nav').style;
+        s.display = s.display === 'block' ? 'none' : 'block';
+    })
+})();
