2e4e05e5d446b04ecb917f324b43500e6eeb3e97
eckon <niklas.meyer95@hotmail.de>
2019-05-26 20:27:32 +0200

    cs-fix

--- main.go
+++ main.go
@@ -1,148 +1,151 @@
 package main
 
 import (
-    "github.com/gorilla/mux"
-    "net/http"
-    "html/template"
-    "fmt"
-    "os"
-    "github.com/gorilla/handlers"
-    "strings"
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
-type pageData struct {
-    HeaderInfo headerInfo
-    Data       interface{}
+type PageData struct {
+	HeaderInfo HeaderInfo
+	Data       interface{}
 }
 
-type headerInfo struct {
-    Title      string
-    Navigation []pathInfo
-    OnHomePage bool
-    User       string
+type HeaderInfo struct {
+	Title      string
+	Navigation []PathInfo
+	OnHomePage bool
+	User       string
 }
 
-type pathInfo struct {
-    Title   string
-    Path    string
-    Class   string
+type PathInfo struct {
+	Title string
+	Path  string
+	Class string
 }
 
 var tpl *template.Template
 
 func init() {
-    // import templates
-    tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+	// import templates
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
 }
 
 func main() {
-    r := mux.NewRouter()
+	r := mux.NewRouter()
 
-    // setup the routes from all places
-    handleAllFunc(r)
-    // fetch all the static data (public: js, css, etc.)
-    r.PathPrefix("/public").Handler(http.StripPrefix("/public", http.FileServer(http.Dir("public"))))
+	// setup the routes from all places
+	handleAllFunc(r)
+	// fetch all the static data (public: js, css, etc.)
+	r.PathPrefix("/public").Handler(http.StripPrefix("/public", http.FileServer(http.Dir("public"))))
 
-    // logging access logs
-    accessLog, err := os.Create("access_log.txt")
-    if err != nil {
-        panic(err)
-    }
-    defer func() {
-        if err := accessLog.Close(); err != nil {
-            panic(err)
-        }
-    }()
+	// logging access logs
+	accessLog, err := os.Create("logs/access.log")
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := accessLog.Close(); err != nil {
+			panic(err)
+		}
+	}()
 
-    // start go routine to redirect every port 80 (http) to 443 (https)
-    go func() {
-       http.ListenAndServe(":80", http.HandlerFunc(func(wr http.ResponseWriter, req *http.Request) {
-           hostDomain := strings.Split(req.Host, ":")[0]
-           http.Redirect(wr, req, "https://" + hostDomain + ":443" + req.URL.Path, http.StatusTemporaryRedirect)
-       }))
-    }()
+	// start go routine to redirect every port 80 (http) to 443 (https)
+	go func() {
+	   http.ListenAndServe(":80", http.HandlerFunc(func(wr http.ResponseWriter, req *http.Request) {
+	       hostDomain := strings.Split(req.Host, ":")[0]
+	       http.Redirect(wr, req, "https://" + hostDomain + ":443" + req.URL.Path, http.StatusTemporaryRedirect)
+	   }))
+	}()
 
-    s := http.Server{
-        Addr:    ":443",
-        Handler: handlers.LoggingHandler(accessLog, r),
-    }
+	s := http.Server{
+		Addr:    ":443",
+		// Addr:    ":80",
+		Handler: handlers.LoggingHandler(accessLog, r),
+	}
 
-    fmt.Println("Running")
-    err = s.ListenAndServeTLS("/etc/letsencrypt/live/eckon.rocks/fullchain.pem",
-        "/etc/letsencrypt/live/eckon.rocks/privkey.pem")
-    if err != nil {
-        panic(err)
-    }
+	fmt.Println("Running")
+	err = s.ListenAndServeTLS("/etc/letsencrypt/live/eckon.rocks/fullchain.pem",
+	    "/etc/letsencrypt/live/eckon.rocks/privkey.pem")
+	if err != nil {
+	    panic(err)
+	}
+	// s.ListenAndServe()
 }
 
 // for ease handle all handler in this func (even from other go files)
 func handleAllFunc(r *mux.Router) {
-    // index path
-    r.HandleFunc("/", indexHandler).Methods("GET")
-    // paths: /phase (in phase.go)
-    handlePhaseFunc(r)
-    // paths: /authentication (in authentication.go)
-    handleAuthenticationFunc(r)
-    // paths: /calendar (in calendar.go)
-    handleCalendarFunc(r)
-    // paths: /release (in release.go)
-    handleReleaseFunc(r)
-    // redirect on 404
-    r.NotFoundHandler = http.HandlerFunc(indexHandler)
+	// index path
+	r.HandleFunc("/", indexHandler).Methods("GET")
+	// paths: /phase (in phase.go)
+	handlePhaseFunc(r)
+	// paths: /authentication (in authentication.go)
+	handleAuthenticationFunc(r)
+	// paths: /calendar (in calendar.go)
+	handleCalendarFunc(r)
+	// paths: /release (in release.go)
+	handleReleaseFunc(r)
+	// redirect on 404
+	r.NotFoundHandler = http.HandlerFunc(indexHandler)
 }
 
 func indexHandler(wr http.ResponseWriter, req *http.Request) {
-    pageData := pageData{
-        HeaderInfo: getHeaderInfo(req),
-    }
-    if err := tpl.ExecuteTemplate(wr, "index.gohtml", pageData); err != nil {
-        http.Error(wr, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	pageData := PageData{
+		HeaderInfo: getHeaderInfo(req),
+	}
+	if err := tpl.ExecuteTemplate(wr, "index.gohtml", pageData); err != nil {
+		http.Error(wr, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 // build the headerInfo so we can use it everywhere and only have one place to edit it
-func getHeaderInfo(req *http.Request) (h headerInfo) {
-    // update the title if the user is already logged in
-    status := pathInfo{"Login", "/authentication", "authentication"}
-    if checkLoginStatus(req) {
-        status.Title = "Logout"
-    }
+func getHeaderInfo(req *http.Request) (h HeaderInfo) {
+	// update the title if the user is already logged in
+	status := PathInfo{"Login", "/authentication", "authentication"}
+	if checkLoginStatus(req) {
+		status.Title = "Logout"
+	}
 
-    // nav-bar information
-    h = headerInfo{
-        Title: "eckon.rocks",
-        Navigation: []pathInfo{
-            {
-                "Phase",
-                "/phase",
-                "",
-            },
-            {
-                "Calendar",
-                "/calendar",
-                "",
-            },
-            {
-                "Release",
-                "/release",
-                "",
-            },
-            status,
-        },
-        OnHomePage: false,
-        User: getCurrentUsername(req),
-    }
+	// nav-bar information
+	h = HeaderInfo{
+		Title: "eckon.rocks",
+		Navigation: []PathInfo{
+			{
+				"Phase",
+				"/phase",
+				"",
+			},
+			{
+				"Calendar",
+				"/calendar",
+				"",
+			},
+			{
+				"Release",
+				"/release",
+				"",
+			},
+			status,
+		},
+		OnHomePage: false,
+		User:       getCurrentUsername(req),
+	}
 
-    for e := range h.Navigation {
-        // if the path is in the headerInfo -> mark it to highlight it in the front end (append class list)
-        if h.Navigation[e].Path == req.URL.String() {
-            h.Navigation[e].Class += " highlighted-content"
-            return
-        }
-    }
-    // if no highlight -> it's on the home page
-    h.OnHomePage = true
+	for e := range h.Navigation {
+		// if the path is in the headerInfo -> mark it to highlight it in the front end (append class list)
+		if h.Navigation[e].Path == req.URL.String() {
+			h.Navigation[e].Class += " highlighted-content"
+			return
+		}
+	}
+	// if no highlight -> it's on the home page
+	h.OnHomePage = true
 
-    return
+	return
 }
--- release.go
+++ release.go
@@ -1,47 +1,48 @@
 package main
 
 import (
-    "github.com/gorilla/mux"
-    "net/http"
-    "encoding/json"
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type JsonData struct {
-    Releases interface{}
+	Releases interface{}
 }
 
 func handleReleaseFunc(r *mux.Router) {
-    r.HandleFunc("/release", releaseHandler).Methods("GET")
+	r.HandleFunc("/release", releaseHandler).Methods("GET")
 }
 
 func releaseHandler(wr http.ResponseWriter, req *http.Request) {
-    pageData := pageData{
-        HeaderInfo: getHeaderInfo(req),
-    }
+	pageData := PageData{
+		HeaderInfo: getHeaderInfo(req),
+	}
 
-    if checkLoginStatus(req) && getCurrentUsername(req) == "eckon" {
-        jsonData := new(JsonData)
-        // id from normal search https://www.discogs.com/de/artist/40027-Disturbed
-        url := "https://api.discogs.com/artists/40027/releases"
-        // id from main_release
-        // https://api.discogs.com/releases/12678059
-        // http://jsonviewer.stack.hu/
-        requestApi(url, jsonData)
-        pageData.Data = jsonData.Releases
-    }
+	if checkLoginStatus(req) && getCurrentUsername(req) == "eckon" {
+		jsonData := new(JsonData)
+		// id from normal search https://www.discogs.com/de/artist/40027-Disturbed
+		url := "https://api.discogs.com/artists/40027/releases"
+		// id from main_release
+		// https://api.discogs.com/releases/12678059
+		// http://jsonviewer.stack.hu/
+		requestApi(url, jsonData)
+		pageData.Data = jsonData.Releases
+	}
 
-    if err := tpl.ExecuteTemplate(wr, "release.gohtml", pageData); err != nil {
-        http.Error(wr, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	if err := tpl.ExecuteTemplate(wr, "release.gohtml", pageData); err != nil {
+		http.Error(wr, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func requestApi(url string, target *JsonData) {
-    res, err := http.Get(url)
-    if err != nil {
-        panic(err)
-    }
-    defer res.Body.Close()
+	res, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
 
-    json.NewDecoder(res.Body).Decode(target)
+	json.NewDecoder(res.Body).Decode(target)
 }
